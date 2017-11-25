4d290685adcd79e91abec1514ab090320ab96e96
admin <dylan.dinh@epitech.eu>
2017-11-25 16:55:04 +0100

    pam auth

--- PAMELA/test.c
+++ PAMELA/test.c
@@ -28,13 +28,19 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
   /* PAM entry point for authentication verification */
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-    return(PAM_IGNORE);
+      const char *user = NULL;
+      int pgu_ret;
+
+      pgu_ret = pam_get_user(pamh, &user, NULL);
+      if (pgu_ret != PAM_SUCCESS || user == NULL)
+          return(PAM_IGNORE);
+      return(PAM_IGNORE);
 }
+
 /*
 PAM entry point for setting user credentials (that is, to actually
 establish the authenticated user's credentials to the service provider)
 */
-
 int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
           return(PAM_IGNORE);
