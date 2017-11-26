9956b950fd651d520b3c651a07c5edfd7e48410f
admin <dylan.dinh@epitech.eu>
2017-11-26 23:14:58 +0100

    lol

--- encrypted_container.sh
+++ encrypted_container.sh
@@ -10,10 +10,10 @@ if [ -f "/home/$USER/secure_data-$USER" ]
           u=/$USER
         fi
         sudo apt-get install cryptsetup
-        dd if=/dev/urandom bs=1M count=50 of=/$u/secure_data-$u
-        sudo cryptsetup luksFormat /$u/secure_data-$u
-        sudo cryptsetup luksOpen /$u/secure_data-$u secure_data-$u
+        dd if=/dev/urandom bs=1M count=50 of=$u/secure_data-$u
+        sudo cryptsetup luksFormat $u/secure_data-$u
+        sudo cryptsetup luksOpen $u/secure_data-$u secure_data-$u
         sudo mkfs.ext4 /dev/mapper/secure_data-$u
-        mkdir /$u/secure_data-rw
+        mkdir $u/secure_data-rw
         echo "/dev/mapper/secure_data-$u /home/$u/secure_data-rw ext4 defaults, noauto 0 0" | sudo tee --append /etc/fstab
     fi
