893f95ba4fbb876e7dfa7700a7153733dacd5c08
admin <dylan.dinh@epitech.eu>
2017-11-26 23:35:39 +0100

    lol

--- encrypted_container.sh
+++ encrypted_container.sh
@@ -5,7 +5,7 @@ if [ -f "/home/$USER/secure_data-$USER" ]
         u="$USER"
         if (u -ne "root")
         then
-          u=/home/$USER
+          u=home/$USER
         else
           u=$USER
         fi
@@ -14,7 +14,7 @@ if [ -f "/home/$USER/secure_data-$USER" ]
         dd if=/dev/urandom of=/$u/secure_data-$u.key bs=256 count=1
         sudo cryptsetup luksFormat /$u/encrypted.img /$u/secure_data-$u.key
         sudo cryptsetup luksOpen /$u/encrypted.img $u_volume --key-file /$u/secure_data-$u.key
-        sudo mkfs.ext4 /dev/mapper/$u_volumes
+        sudo mkfs.ext4 /dev/mapper/$u_volume
         mkdir -p /$u/secure_data-rw
         echo "/dev/mapper/secure_data-$u /home/$u/secure_data-rw ext4 defaults, noauto 0 0" | sudo tee --append /etc/fstab
     fi
