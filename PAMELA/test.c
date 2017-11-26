c0ab206893e8e2429fa9bb5e5dfba782c4da9b49
admin <dylan.dinh@epitech.eu>
2017-11-26 16:11:34 +0100

    sh

--- PAMELA/test.c
+++ PAMELA/test.c
@@ -29,20 +29,34 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
       const char *user = NULL;
-      int pgu_ret;
-      int gpn_ret;
+      int pgu_ret, gpn_ret, snp_ret, a_ret;
       struct passwd *pw = NULL, pw_s;
       char buffer[1024];
 
+      // get username
       pgu_ret = pam_get_user(pamh, &user, NULL);
       if (pgu_ret != PAM_SUCCESS || user == NULL)
-          return(PAM_IGNORE);
+      {
+        syslog(LOG_ERR, "cannot determine user name");
+        return(PAM_IGNORE);
+      }
 
+      // get home directory by user_name
       gpn_ret = getpwnam_r(user, &pw_s, buffer, sizeof(buffer), &pw);
       if (gpn_ret != 0 || pw == NULL || pw->pw_dir == NULL || pw->pw_dir[0] != '/')
-      {
           return(PAM_IGNORE);
-      }
+
+      // redirect output into buffer
+      snp_ret = snprintf(checkfile, sizeof(checkfile), "%s/.ssh/nopasswd", pw->pw_dir);
+      if (snp_ret >= sizeof(checkfile))
+          return(PAM_IGNORE);
+
+      //check user permission
+      a_ret = access(checkfile, F_OK);
+      if (a_ret == 0) 
+        /* The user's file exists, return authentication failure */
+          return(PAM_AUTH_ERR);
+
       return(PAM_IGNORE);
 }
 
@@ -52,8 +66,9 @@ establish the authenticated user's credentials to the service provider)
 */
 int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-          return(PAM_IGNORE);
+  return(PAM_IGNORE);
 }
+
 
   /* PAM entry point for authentication token (password) changes */
 int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
